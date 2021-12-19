fe1a527120c72571438cc5b0d56ac0273e784482
Garnet3106 <manage@gant.work>
2021-12-19 13:13:05 +0900

    fix: stack structure

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,8 +1,9 @@
 use std::mem::size_of;
+use std::slice::from_raw_parts;
 
 use crate::bytecode::*;
 
-use libc::*;
+use libc::{c_void, malloc, free};
 
 use rustnutlib::*;
 use rustnutlib::console::*;
@@ -11,6 +12,7 @@ use rustnutlib::file::*;
 pub enum RuntimeError {
     Unknown {},
     FileError { err: FileError },
+    ArithmeticOverflow {},
     IndexOutOfBytecodeRange {},
     InvalidHeaderSize {},
     InvalidMagicNumber {},
@@ -24,6 +26,7 @@ impl ConsoleLogger for RuntimeError {
         return match self {
             RuntimeError::Unknown {} => log!(Error, "unknown error"),
             RuntimeError::FileError { err } => err.get_log(),
+            RuntimeError::ArithmeticOverflow {} => log!(Error, &format!("arithmetic overflow")),
             RuntimeError::IndexOutOfBytecodeRange {} => log!(Error, &format!("index out of bytecode range")),
             RuntimeError::InvalidHeaderSize {} => log!(Error, &format!("invalid header size")),
             RuntimeError::InvalidMagicNumber {} => log!(Error, &format!("invalid magic number")),
@@ -36,10 +39,14 @@ impl ConsoleLogger for RuntimeError {
 
 pub type RuntimeResult<T> = Result<T, RuntimeError>;
 
+// note: スタックサイズ: Byte 単位
 pub struct Interpreter {
-    bytecode: Box<Vec<u8>>,
-    program_counter: usize,
-    stack: *mut u64,
+    bytecode_origin: *mut u8,
+    bytecode_top: *mut u8,
+    bytecode_len: usize,
+    bytecode_counter: usize,
+    stack_origin: *mut u8,
+    stack_top: *mut u8,
     stack_counter: usize,
     max_stack_size: usize,
     stack_addition_size: usize,
@@ -52,113 +59,151 @@ impl Interpreter {
             return Err(RuntimeError::StackOverflow {});
         }
 
+        let mut bytecode_bytes = bytecode.into_vec();
+        let bytecode_origin = bytecode_bytes.as_mut_ptr();
+        let stack_origin = malloc(size_of::<u8>() * stack_addition_size * 1000) as *mut u8;
+
         let interpreter = Interpreter {
-            bytecode: bytecode.into_vec(),
-            program_counter: 127,
-            stack: malloc(size_of::<u8>() * stack_addition_size * 1000) as *mut u64,
+            bytecode_origin: bytecode_origin,
+            bytecode_top: bytecode_origin.add(128),
+            bytecode_len: bytecode_bytes.len(),
+            bytecode_counter: 128,
+            stack_origin: stack_origin,
+            stack_top: stack_origin,
             stack_counter: 0,
-            max_stack_size: max_stack_size,
+            max_stack_size: max_stack_size * 1000,
             stack_addition_size: stack_addition_size,
         };
 
         return Ok(interpreter);
     }
 
-    // arg: max_stack_size: KB 単位
     pub unsafe fn run(&mut self) -> RuntimeResult<()> {
-        // println!("add {}", *stack.add(1));
-        // *stack = 0xabcd;
-        // println!("value {:0x}", *stack as u8);
+        macro_rules! add {
+            ($ty:ty, $f:ident) => {
+                {
+                    let v1 = *self.pop::<$ty>();
+                    let v2 = *self.pop::<$ty>();
+                    let (v3, is_overflowed) = v1.$f(v2);
+
+                    if is_overflowed {
+                        return Err(RuntimeError::ArithmeticOverflow {});
+                    }
+
+                    self.push::<$ty>(v3);
+                }
+            };
+        }
+
+        macro_rules! push_next {
+            ($ty:ty) => {
+                {
+                    let value = *self.next::<$ty>();
+                    self.push::<$ty>(value);
+                }
+            };
+        }
 
         loop {
-            // 終了命令がないまま EOF に到達した場合
-            if self.program_counter >= self.bytecode.len() {
-                return Err(RuntimeError::InvalidTermination {});
-            }
+            let opcode = *self.next::<u8>();
 
-            let opcode = self.next_byte();
-            println!("opcode 0x{:0x}", opcode);
+            println!("opcode 0x{:0x} {:?}", opcode, self.to_bytecode_vec());
+
             match opcode {
                 // RET
-                0x00 => (),
+                0x00 => {
+                    let ret_value = self.pop::<u64>();
+                    println!("\treturn");
+                    println!("\targument: {:0x}", ret_value);
+                },
                 // EXIT
                 0x01 => break,
-                // PUSH_8
-                0x02 => {
-                    let v = self.next_byte() as u64;
-                    println!("\tpush {:?}", std::slice::from_raw_parts(self.stack, self.stack_counter));
-                    self.push(v);
-                    println!("\tpush {:?}", std::slice::from_raw_parts(self.stack, self.stack_counter));
-                },
-                // POP
-                0x03 => {
-                    let _ = self.pop();
+                // PUSH_32
+                0x02 => push_next!(u32),
+                // PUSH_64
+                0x03 => push_next!(u64),
+                // POP_32
+                0x04 => {
+                    let _ = self.pop::<u32>();
                     ()
                 },
-                // IADD_8
-                0x04 => {
-                    println!("\tpop {:?}", std::slice::from_raw_parts(self.stack, self.stack_counter));
-                    let v1 = self.pop();
-                    println!("\t{}", v1);
-                    println!("\tpop {:?}", std::slice::from_raw_parts(self.stack, self.stack_counter));
-                    let v2 = self.pop();
-                    println!("\t{}", v2);
-                    println!("\tpop {:?}", std::slice::from_raw_parts(self.stack, self.stack_counter));
-                    println!("\t{} + {} = {}", v1, v2, v1 + v2);
-                    self.push(v1 + v2);
+                // POP_64
+                0x05 => {
+                    let _ = self.pop::<u64>();
+                    ()
                 },
-                _ => return Err(RuntimeError::UnknownOpcode {})
+                // IADD_32
+                0x06 => add!(u32, overflowing_add),
+                // IADD_64
+                0x07 => add!(u64, overflowing_add),
+                _ => return Err(RuntimeError::UnknownOpcode {}),
             }
         }
 
-        println!("");
-        println!("- stack -");
-        println!("");
-        println!("{:?}", std::slice::from_raw_parts(self.stack, self.stack_counter));
-        println!("{:?}", std::slice::from_raw_parts(self.stack, 5));
+        println!();
+        println!("stack result: {:?}", from_raw_parts(self.stack_origin, self.stack_counter));
+        println!();
 
-        self.terminate();
         return Ok(());
     }
 
-    // note: メモリリークを防ぐため終了時は必ず呼び出すこと
-    unsafe fn terminate(&mut self) {
-        free(self.stack as *mut c_void);
+    unsafe fn to_bytecode_vec(&mut self) -> Vec<u8> {
+        return from_raw_parts(self.stack_origin, self.stack_counter).to_vec();
+    }
+
+    unsafe fn next<T>(&mut self) -> &T {
+        if self.bytecode_counter > self.bytecode_len {
+            panic!("unexpected end of bytecode");
+        }
+
+        let size = size_of::<T>();
+        self.bytecode_counter += size;
+
+        let tmp_bytecode = self.bytecode_top as *mut T;
+        let value = &*tmp_bytecode;
+        self.bytecode_top = tmp_bytecode as *mut u8;
+
+        self.bytecode_top = self.bytecode_top.add(size);
+
+        return value;
     }
 
     #[inline(always)]
-    unsafe fn push(&mut self, value: u64) {
-        if self.stack_counter + 1 >= self.max_stack_size {
+    unsafe fn push<T>(&mut self, value: T) {
+        let value_size = size_of::<T>();
+
+        if self.stack_counter + value_size > self.max_stack_size {
             panic!("stack access violation (stack overflow)");
         }
 
-        println!("\tpush value {} {}", value, *self.stack);
+        self.stack_counter += value_size;
 
-        if self.stack_counter == 0 {
-            *self.stack = value;
-        } else {
-            let a = self.stack.add(1);
-            *a = value;
-        }
+        let tmp_stack = self.stack_top as *mut T;
+        *tmp_stack = value;
 
-        self.stack_counter += 1;
+        self.stack_top = self.stack_top.add(size_of::<T>());
     }
 
     #[inline(always)]
-    unsafe fn pop(&mut self) -> u64 {
-        if self.stack_counter == 0 {
+    unsafe fn pop<T>(&mut self) -> &T {
+        let value_size = size_of::<T>();
+
+        if self.stack_counter < value_size {
             panic!("stack access violation");
         }
 
-        let pop_value = *self.stack;
-        self.stack_counter -= 1;
-        let pop_value = self.stack.offset(-1);
-        return *pop_value;
-    }
+        self.stack_counter -= value_size;
 
-    #[inline(always)]
-    fn next_byte(&mut self) -> u8 {
-        self.program_counter += 1;
-        return self.bytecode[self.program_counter];
+        self.stack_top = self.stack_top.sub(size_of::<T>());
+        let pop_value = self.stack_top as *mut T;
+        return &*pop_value;
+    }
+}
+
+impl Drop for Interpreter {
+    fn drop(&mut self) {
+        unsafe {
+            free(self.stack_origin as *mut c_void);
+        }
     }
 }
