ea0fae078aba0c88dbb3907ffb786093d80b9965
Garnet3106 <manage@gant.work>
2021-12-19 16:14:12 +0900

    add: DynamicHeap

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,7 +1,10 @@
+use std::fmt::{Formatter, Display};
 use std::mem::size_of;
 use std::slice::from_raw_parts;
 
 use crate::bytecode::*;
+
+use colored::*;
 
 use libc::{c_void, malloc, free};
 
@@ -39,58 +42,221 @@ impl ConsoleLogger for RuntimeError {
 
 pub type RuntimeResult<T> = Result<T, RuntimeError>;
 
-// note: スタックサイズ: Byte 単位
+pub struct DynamicHeap {
+    origin_ptr: *mut c_void,
+    top_ptr: *mut c_void,
+    size: usize,
+    max_size: usize,
+    additional_size: usize,
+    counter: usize,
+    offset: usize,
+    is_freed: bool,
+}
+
+impl DynamicHeap {
+    // arg: スタックサイズ: Byte 単位
+    pub unsafe fn new(max_size: usize, additional_size: usize) -> DynamicHeap {
+        let size = size_of::<u8>() * additional_size;
+        let raw_ptr = malloc(size);
+
+        return DynamicHeap::from(raw_ptr, size, 0, max_size, additional_size);
+    }
+
+    pub unsafe fn from(raw_ptr: *mut c_void, size: usize, offset: usize, max_size: usize, additional_size: usize) -> DynamicHeap {
+        if offset > size || additional_size > max_size {
+            panic!("{}", "stack overflow on dynamic heap".on_red());
+        }
+
+        return DynamicHeap {
+            origin_ptr: raw_ptr,
+            top_ptr: raw_ptr.add(offset),
+            size: size,
+            max_size: max_size,
+            additional_size: additional_size,
+            counter: offset,
+            offset: offset,
+            is_freed: false,
+        }
+    }
+
+    fn check_freed(&self) {
+        if self.is_freed {
+            panic!("{}", "heap access violation: use of freed area".on_red());
+        }
+    }
+
+    unsafe fn free(&mut self) {
+        self.check_freed();
+
+        free(self.origin_ptr);
+        self.is_freed = true;
+    }
+
+    unsafe fn next<T>(&mut self) -> &T {
+        self.check_freed();
+
+        let value_size = size_of::<T>();
+
+        if self.counter + value_size > self.size {
+            panic!("{}", "dynamic heap access violation".on_red());
+        }
+
+        self.counter += value_size;
+
+        let tmp_ptr = self.top_ptr as *mut T;
+        let value = &*tmp_ptr;
+        self.top_ptr = (tmp_ptr as *mut c_void).add(value_size);
+
+        return value;
+    }
+
+    pub unsafe fn pop<T>(&mut self) -> &T {
+        self.check_freed();
+
+        let value_size = size_of::<T>();
+
+        if self.counter < value_size {
+            panic!("{}", "stack access violation".on_red());
+        }
+
+        self.counter -= value_size;
+
+        self.top_ptr = self.top_ptr.sub(size_of::<T>());
+        let pop_value = self.top_ptr as *mut T;
+        return &*pop_value;
+    }
+
+    pub unsafe fn push<T>(&mut self, value: T) {
+        self.check_freed();
+
+        let value_size = size_of::<T>();
+
+        if self.counter + value_size > self.max_size {
+            panic!("{}", "stack access violation (stack overflow)".on_red());
+        }
+
+        let tmp_stack = self.top_ptr as *mut T;
+        *tmp_stack = value;
+
+        self.counter += value_size;
+        self.top_ptr = self.top_ptr.add(size_of::<T>());
+    }
+
+    pub fn len(&self) -> usize {
+        self.check_freed();
+
+        return self.counter;
+    }
+
+    pub unsafe fn to_bytes(&self) -> Vec<u8> {
+        self.check_freed();
+
+        return from_raw_parts(self.origin_ptr as *const u8, self.counter)[self.offset..].to_vec();
+    }
+}
+
+impl Display for DynamicHeap {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        unsafe {
+            let mut i = 0usize;
+            let bytes = self.to_bytes();
+
+            let s = if bytes.len() != 0 {
+                bytes.iter().map(|v| {
+                    let div = if i != 0 && i % 8 == 0 { "|\n" } else { "" };
+                    i += 1;
+                    format!("{}{:0x} ", div, v)
+                }).collect::<Vec<String>>().join("")
+            } else {
+                "<empty>".to_string()
+            };
+
+            return write!(f, "{}", s);
+        }
+    }
+}
+
+pub enum OpcodeKind {
+    Unknown,
+    Ret,
+    Exit,
+    Push32,
+    Push64,
+    Pop32,
+    Pop64,
+    IAdd32,
+    IAdd64,
+}
+
+impl Display for OpcodeKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            OpcodeKind::Unknown => "unknown",
+            OpcodeKind::Ret => "ret",
+            OpcodeKind::Exit => "exit",
+            OpcodeKind::Push32 => "push_32",
+            OpcodeKind::Push64 => "push_64",
+            OpcodeKind::Pop32 => "pop_32",
+            OpcodeKind::Pop64 => "pop_64",
+            OpcodeKind::IAdd32 => "iadd_32",
+            OpcodeKind::IAdd64 => "iadd_64",
+        };
+
+        return write!(f, "{}", s);
+    }
+}
+
+impl From<u8> for OpcodeKind {
+    fn from(value: u8) -> OpcodeKind {
+        return match value {
+            0x00 => OpcodeKind::Ret,
+            0x01 => OpcodeKind::Exit,
+            0x02 => OpcodeKind::Push32,
+            0x03 => OpcodeKind::Push64,
+            0x04 => OpcodeKind::Pop32,
+            0x05 => OpcodeKind::Pop64,
+            0x06 => OpcodeKind::IAdd32,
+            0x07 => OpcodeKind::IAdd64,
+            _ => OpcodeKind::Unknown,
+        };
+    }
+}
+
 pub struct Interpreter {
-    bytecode_origin: *mut u8,
-    bytecode_top: *mut u8,
-    bytecode_len: usize,
-    bytecode_counter: usize,
-    stack_origin: *mut u8,
-    stack_top: *mut u8,
-    stack_counter: usize,
-    max_stack_size: usize,
-    stack_addition_size: usize,
+    bytecode: DynamicHeap,
+    stack: DynamicHeap,
 }
 
 impl Interpreter {
     pub unsafe fn launch(bytecode: Bytecode, max_stack_size: usize, stack_addition_size: usize) -> RuntimeResult<Interpreter> {
-        // note: スタックの初期サイズが最大サイズを超えている場合
-        if stack_addition_size > max_stack_size {
-            return Err(RuntimeError::StackOverflow {});
-        }
-
         let mut bytecode_bytes = bytecode.into_vec();
-        let bytecode_origin = bytecode_bytes.as_mut_ptr();
-        let stack_origin = malloc(size_of::<u8>() * stack_addition_size * 1000) as *mut u8;
+        let bytecode_heap = DynamicHeap::from(bytecode_bytes.as_mut_ptr() as *mut c_void, bytecode_bytes.len(), 128, 1024 * 1024, 1024);
+
+        let stack = DynamicHeap::new(max_stack_size, stack_addition_size);
 
         let interpreter = Interpreter {
-            bytecode_origin: bytecode_origin,
-            bytecode_top: bytecode_origin.add(128),
-            bytecode_len: bytecode_bytes.len(),
-            bytecode_counter: 128,
-            stack_origin: stack_origin,
-            stack_top: stack_origin,
-            stack_counter: 0,
-            max_stack_size: max_stack_size * 1000,
-            stack_addition_size: stack_addition_size,
+            bytecode: bytecode_heap,
+            stack: stack,
         };
 
         return Ok(interpreter);
     }
 
     pub unsafe fn run(&mut self) -> RuntimeResult<()> {
-        macro_rules! add {
+        macro_rules! calc {
             ($ty:ty, $f:ident) => {
                 {
-                    let v1 = *self.pop::<$ty>();
-                    let v2 = *self.pop::<$ty>();
+                    let v1 = *self.stack.pop::<$ty>();
+                    let v2 = *self.stack.pop::<$ty>();
                     let (v3, is_overflowed) = v1.$f(v2);
+
+                    println!("operand {}, {}", v1, v2);
 
                     if is_overflowed {
                         return Err(RuntimeError::ArithmeticOverflow {});
                     }
 
-                    self.push::<$ty>(v3);
+                    self.stack.push::<$ty>(v3);
                 }
             };
         }
@@ -98,112 +264,56 @@ impl Interpreter {
         macro_rules! push_next {
             ($ty:ty) => {
                 {
-                    let value = *self.next::<$ty>();
-                    self.push::<$ty>(value);
+                    let value = *self.bytecode.next::<$ty>();
+                    self.stack.push::<$ty>(value);
                 }
             };
         }
 
         loop {
-            let opcode = *self.next::<u8>();
+            let opcode = *self.bytecode.next::<u8>();
+            let opcode_kind = OpcodeKind::from(opcode);
 
-            println!("opcode 0x{:0x} {:?}", opcode, self.to_bytecode_vec());
+            println!("{}", format!("{} (0x{:0x})", opcode_kind, opcode).blue());
+            println!("{}", self.stack.to_string().bright_black());
+            println!();
 
-            match opcode {
-                // RET
-                0x00 => {
-                    let ret_value = self.pop::<u64>();
-                    println!("\treturn");
-                    println!("\targument: {:0x}", ret_value);
+            match opcode_kind {
+                OpcodeKind::Ret => {
+                    let ret_value = self.stack.pop::<u32>();
+                    println!("ret_value {:0x}", ret_value);
                 },
-                // EXIT
-                0x01 => break,
-                // PUSH_32
-                0x02 => push_next!(u32),
-                // PUSH_64
-                0x03 => push_next!(u64),
-                // POP_32
-                0x04 => {
-                    let _ = self.pop::<u32>();
+                OpcodeKind::Exit => break,
+                OpcodeKind::Push32 => push_next!(u32),
+                OpcodeKind::Push64 => push_next!(u64),
+                OpcodeKind::Pop32 => {
+                    let _ = self.stack.pop::<u32>();
                     ()
                 },
-                // POP_64
-                0x05 => {
-                    let _ = self.pop::<u64>();
+                OpcodeKind::Pop64 => {
+                    let _ = self.stack.pop::<u64>();
                     ()
                 },
-                // IADD_32
-                0x06 => add!(u32, overflowing_add),
-                // IADD_64
-                0x07 => add!(u64, overflowing_add),
+                OpcodeKind::IAdd32 => calc!(u32, overflowing_add),
+                OpcodeKind::IAdd64 => calc!(u64, overflowing_add),
                 _ => return Err(RuntimeError::UnknownOpcode {}),
             }
+
+            println!();
         }
 
-        println!();
-        println!("stack result: {:?}", from_raw_parts(self.stack_origin, self.stack_counter));
-        println!();
+        if self.stack.len() != 0 {
+            panic!("{}", "unconsumed stack element".on_red());
+        }
 
         return Ok(());
-    }
-
-    unsafe fn to_bytecode_vec(&mut self) -> Vec<u8> {
-        return from_raw_parts(self.stack_origin, self.stack_counter).to_vec();
-    }
-
-    unsafe fn next<T>(&mut self) -> &T {
-        if self.bytecode_counter > self.bytecode_len {
-            panic!("unexpected end of bytecode");
-        }
-
-        let size = size_of::<T>();
-        self.bytecode_counter += size;
-
-        let tmp_bytecode = self.bytecode_top as *mut T;
-        let value = &*tmp_bytecode;
-        self.bytecode_top = tmp_bytecode as *mut u8;
-
-        self.bytecode_top = self.bytecode_top.add(size);
-
-        return value;
-    }
-
-    #[inline(always)]
-    unsafe fn push<T>(&mut self, value: T) {
-        let value_size = size_of::<T>();
-
-        if self.stack_counter + value_size > self.max_stack_size {
-            panic!("stack access violation (stack overflow)");
-        }
-
-        self.stack_counter += value_size;
-
-        let tmp_stack = self.stack_top as *mut T;
-        *tmp_stack = value;
-
-        self.stack_top = self.stack_top.add(size_of::<T>());
-    }
-
-    #[inline(always)]
-    unsafe fn pop<T>(&mut self) -> &T {
-        let value_size = size_of::<T>();
-
-        if self.stack_counter < value_size {
-            panic!("stack access violation");
-        }
-
-        self.stack_counter -= value_size;
-
-        self.stack_top = self.stack_top.sub(size_of::<T>());
-        let pop_value = self.stack_top as *mut T;
-        return &*pop_value;
     }
 }
 
 impl Drop for Interpreter {
     fn drop(&mut self) {
         unsafe {
-            free(self.stack_origin as *mut c_void);
+            self.stack.free();
         }
     }
 }
