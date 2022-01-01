b792a54e6786e9f758ad56d734e11181ea22e207
Garnet3106 <manage@gant.work>
2022-01-01 10:30:29 +0900

    fix: remake the machine

--- src/runtime.rs
+++ src/runtime.rs
@@ -61,21 +61,6 @@ pub enum Opcode {
     Unknown,
     Nop,
     Exit,
-    Invoke,
-    Ret,
-    RetVal,
-    Push32,
-    Push64,
-    Pop32,
-    Pop64,
-    IAdd32,
-    IAdd64,
-    ISub32,
-    ISub64,
-    IMul32,
-    IMul64,
-    IDiv32,
-    IDiv64,
 }
 
 impl Display for Opcode {
@@ -84,21 +69,6 @@ impl Display for Opcode {
             Opcode::Unknown => "unknown",
             Opcode::Nop => "nop",
             Opcode::Exit => "exit",
-            Opcode::Invoke => "invoke",
-            Opcode::Ret => "ret",
-            Opcode::RetVal => "retval",
-            Opcode::Push32 => "push_32",
-            Opcode::Push64 => "push_64",
-            Opcode::Pop32 => "pop_32",
-            Opcode::Pop64 => "pop_64",
-            Opcode::IAdd32 => "iadd_32",
-            Opcode::IAdd64 => "iadd_64",
-            Opcode::ISub32 => "isub_32",
-            Opcode::ISub64 => "isub_64",
-            Opcode::IMul32 => "imul_32",
-            Opcode::IMul64 => "imul_64",
-            Opcode::IDiv32 => "idiv_32",
-            Opcode::IDiv64 => "idiv_64",
         };
 
         return write!(f, "{}", s);
@@ -110,21 +80,6 @@ impl From<u8> for Opcode {
         return match value {
             0x00 => Opcode::Nop,
             0x01 => Opcode::Exit,
-            0x02 => Opcode::Invoke,
-            0x03 => Opcode::Ret,
-            0x04 => Opcode::RetVal,
-            0x05 => Opcode::Push32,
-            0x06 => Opcode::Push64,
-            0x07 => Opcode::Pop32,
-            0x08 => Opcode::Pop64,
-            0x09 => Opcode::IAdd32,
-            0x0a => Opcode::IAdd64,
-            0x0b => Opcode::ISub32,
-            0x0c => Opcode::ISub64,
-            0x0d => Opcode::IMul32,
-            0x0e => Opcode::IMul64,
-            0x0f => Opcode::IDiv32,
-            0x10 => Opcode::IDiv64,
             _ => Opcode::Unknown,
         };
     }
@@ -152,32 +107,30 @@ impl Interpreter {
         let bytecode_len = bytecode_bytes.len();
         let mut bytecode_ptr = bytecode_bytes.as_mut_ptr() as *mut c_void;
 
-        let mut link_area_ptr = bytecode_ptr;
-        let link_area_offset = 128usize;
-        link_area_ptr = link_area_ptr.add(link_area_offset);
-        let link_element_size = 16;
+        let pool_offset = 128usize;
+        let mut pool_ptr = bytecode_ptr.add(pool_offset);
 
-        let entry_point_index = *(link_area_ptr as *mut usize);
-        bytecode_ptr = bytecode_ptr.add(entry_point_index);
+        let entry_point_func_index = *(bytecode_ptr.add(*(pool_ptr as *mut usize) + 1) as *mut usize);
+        let entry_point_pc = *(bytecode_ptr.add(entry_point_func_index) as *mut usize);
+        let mut inst_ptr = bytecode_ptr.add(entry_point_pc);
 
         let max_stack_size = 1024usize;
         let mut stack_ptr = malloc(max_stack_size) as *mut c_void;
-        let mut call_stack_ptr = malloc(max_stack_size) as *mut c_void;
 
-        // todo: 接頭辞 r_
         // note: Exit Status
         let es;
         // note: Stack Pointer
         let mut sp = 0usize;
-        // note: Call Stack Pointer
-        let mut csp = 0usize;
         // note: Program Counter
-        let mut pc = entry_point_index;
-        // note: Link Area Pointer
-        let mut lap = link_area_offset;
+        let mut pc = entry_point_pc;
+        // note: Pool Pointer
+        let mut pp = pool_offset;
+
+        println!("{}", pc);
 
         // note: エントリポイント用のコールスタック要素をプッシュ
 
+        /*
         // * リターンアドレス
         let tmp_ptr = call_stack_ptr as *mut usize;
         *tmp_ptr = bytecode_len - 1;
@@ -194,6 +147,7 @@ impl Interpreter {
         call_stack_ptr = call_stack_ptr.add(size_of::<u32>());
 
         csp += size_of::<usize>() + size_of::<u32>() * 2;
+        */
 
         macro_rules! raw_ptr_to_string {
             ($ptr:expr, $size:expr) => {
@@ -218,31 +172,6 @@ impl Interpreter {
         }
 
         'operator: loop {
-            macro_rules! jump_to {
-                ($ptr:expr, $curr_pos:expr, $jump_to:expr, $size:expr, $err_status:expr) => {
-                    {
-                        if $jump_to >= $size {
-                            exit!($err_status);
-                        }
-
-                        $ptr = $ptr.offset($jump_to as isize - $curr_pos as isize);
-                        $curr_pos = $jump_to;
-                    }
-                };
-            }
-
-            macro_rules! jump_to_link_elem {
-                ($link_num:expr) => {
-                    jump_to!(link_area_ptr, lap, link_area_offset + $link_num * link_element_size, bytecode_len, ExitStatus::BytecodeAccessViolation)
-                };
-            }
-
-            macro_rules! jump_bytecode_to {
-                ($address:expr) => {
-                    jump_to!(bytecode_ptr, pc, $address, bytecode_len, ExitStatus::BytecodeAccessViolation)
-                };
-            }
-
             macro_rules! next {
                 ($ptr:expr, $curr_pos:expr, $ty:ty, $size:expr, $err_status:expr) => {
                     {
@@ -262,249 +191,9 @@ impl Interpreter {
                 };
             }
 
-            macro_rules! next_bytecode {
+            macro_rules! next_prg {
                 ($ty:ty) => {
-                    next!(bytecode_ptr, pc, $ty, bytecode_len, ExitStatus::BytecodeAccessViolation)
-                };
-            }
-
-            macro_rules! next_link_area {
-                ($ty:ty) => {
-                    next!(link_area_ptr, lap, $ty, bytecode_len, ExitStatus::BytecodeAccessViolation)
-                };
-            }
-
-            macro_rules! top {
-                ($ptr:expr, $counter:expr, $ty:ty, $err_status:expr) => {
-                    {
-                        let value_size = size_of::<$ty>();
-
-                        if $counter < value_size {
-                            exit!($err_status);
-                        }
-
-                        *($ptr as *mut $ty).sub(1)
-                    }
-                };
-            }
-
-            macro_rules! call_stack_top {
-                ($ty:ty) => {
-                    top!(call_stack_ptr, csp, $ty, ExitStatus::CallStackOverflow)
-                };
-            }
-
-            macro_rules! push {
-                ($ptr:expr, $curr_pos:expr, $ty:ty, $value:expr, $size:expr, $err_status:expr) => {
-                    {
-                        let value_size = size_of::<$ty>();
-
-                        if $curr_pos + value_size > $size {
-                            exit!($err_status);
-                        }
-
-                        let tmp_ptr = $ptr as *mut $ty;
-                        *tmp_ptr = $value;
-
-                        $curr_pos += value_size;
-                        $ptr = $ptr.add(value_size);
-                    }
-                };
-            }
-
-            macro_rules! push_stack {
-                ($ty:ty, $value:expr $(, $count_fsp:expr)?) => {
-                    {
-                        #[allow(unused_assignments, unused_mut)]
-                        let mut count = true;
-                        $(count = $count_fsp;)?
-
-                        if count {
-                            add_fsp!(size_of::<$ty>() as u32);
-                        }
-
-                        push!(stack_ptr, sp, $ty, $value, max_stack_size, ExitStatus::StackOverflow)
-                    }
-                };
-            }
-
-            macro_rules! push_call_stack {
-                ($ty:ty, $value:expr) => {
-                    push!(call_stack_ptr, csp, $ty, $value, max_stack_size, ExitStatus::CallStackOverflow)
-                };
-            }
-
-            macro_rules! push_stack_next {
-                ($ty:ty) => {
-                    {
-                        let value = next_bytecode!($ty);
-                        push_stack!($ty, value);
-                    }
-                };
-            }
-
-            macro_rules! pop {
-                ($ptr:expr, $curr_pos:expr, $ty:ty, $err_status:expr) => {
-                    {
-                        let value_size = size_of::<$ty>();
-
-                        if $curr_pos < value_size {
-                            exit!($err_status);
-                        }
-
-                        $curr_pos -= value_size;
-                        $ptr = $ptr.sub(value_size);
-
-                        *($ptr as *mut $ty)
-                    }
-                };
-            }
-
-            macro_rules! pop_stack {
-                ($ty:ty $(, $count_fsp:expr)?) => {
-                    {
-                        #[allow(unused_assignments, unused_mut)]
-                        let mut count = true;
-                        $(count = $count_fsp;)?
-
-                        if count {
-                            sub_fsp!(size_of::<$ty>() as u32);
-                        }
-
-                        pop!(stack_ptr, sp, $ty, ExitStatus::StackAccessViolation)
-                    }
-                };
-            }
-
-            macro_rules! pop_call_stack {
-                ($ty:ty) => {
-                    pop!(call_stack_ptr, csp, $ty, ExitStatus::CallStackAccessViolation)
-                };
-            }
-
-            /// コールスタックのスタックポインタ (fsp) を加算
-            macro_rules! add_fsp {
-                ($size:expr) => {
-                    {
-                        let fsp = pop_call_stack!(u32);
-                        push_call_stack!(u32, fsp + $size);
-                    }
-                };
-            }
-
-            /// コールスタックのスタックポインタ (fsp) を減算
-            macro_rules! sub_fsp {
-                ($size:expr) => {
-                    let fsp = pop_call_stack!(u32);
-
-                    if fsp < $size {
-                        exit!(ExitStatus::StackAccessViolation);
-                    }
-
-                    push_call_stack!(u32, fsp - $size);
-                };
-            }
-
-            macro_rules! invoke {
-                () => {
-                    {
-                        let link_num = next_bytecode!(usize);
-                        jump_to_link_elem!(link_num);
-                        let ret_addr = pc;
-                        let start_addr = next_link_area!(usize);
-                        let arg_len = next_link_area!(u32);
-
-                        println!("{}", format!("[link number 0x{:0x} / start at 0x{:0x} / return to 0x{:0x} / {} arguments]", link_num, start_addr, ret_addr, arg_len).bright_green().dimmed());
-                        println!();
-
-                        println!("{}", "call stack (prev):".bright_black());
-                        println!("{}", raw_ptr_to_string!(call_stack_ptr.sub(csp), csp).bright_black());
-                        println!();
-
-                        // note: 先に呼び出し元のコールスタックから引数分の fsp を減算
-                        sub_fsp!(size_of::<usize>() as u32 * arg_len);
-
-                        // note: コールスタックにリターンアドレスをプッシュ
-                        push_call_stack!(usize, ret_addr);
-
-                        if sp < arg_len as usize * size_of::<u32>() {
-                            exit!(ExitStatus::StackAccessViolation);
-                        }
-
-                        // note: 引数をコールスタックへコピー
-                        for i in 0..arg_len as usize {
-                            let value = *((stack_ptr as *mut usize).sub(arg_len as usize - i));
-                            push_call_stack!(usize, value);
-                        }
-
-                        // note: スタックから引数分の要素をポップ; コールスタック操作後のため fsp を変更しない
-                        for _ in 0..arg_len {
-                            pop_stack!(usize, false);
-                        }
-
-                        // note: コールスタックに引数サイズをプッシュ
-                        push_call_stack!(u32, arg_len);
-                        // note: コールスタックにスタックポインタをプッシュ
-                        push_call_stack!(u32, 0);
-
-                        println!("{}", "call stack (modded):".bright_black());
-                        println!("{}", raw_ptr_to_string!(call_stack_ptr.sub(csp), csp).bright_black());
-                        println!();
-
-                        jump_bytecode_to!(start_addr);
-                    }
-                };
-            }
-
-            macro_rules! ret {
-                ($ret_value:expr) => {
-                    {
-                        let ret_value = if $ret_value {
-                            // note: スタックから事前に返り値をポップ
-                            pop_stack!(usize)
-                        } else {
-                            0
-                        };
-
-                        let fsp = call_stack_top!(u32);
-
-                        println!("{}", format!("[fsp={}]", fsp).bright_green().dimmed());
-                        println!();
-
-                        println!("{}", "call stack (prev):".bright_black());
-                        println!("{}", raw_ptr_to_string!(call_stack_ptr.sub(csp), csp).bright_black());
-                        println!();
-
-                        for _ in 0..fsp {
-                            pop_stack!(u8);
-                        }
-
-                        // note: コールスタックから fsp をポップ
-                        pop_call_stack!(u32);
-
-                        let arg_len = pop_call_stack!(u32);
-
-                        for _ in 0..arg_len {
-                            pop_call_stack!(usize);
-                        }
-
-                        let ret_addr = pop_call_stack!(usize);
-
-                        println!("{}", "call stack (modded):".bright_black());
-                        println!("{}", raw_ptr_to_string!(call_stack_ptr.sub(csp), csp).bright_black());
-                        println!();
-
-                        let ret_value_str = if $ret_value { format!(" / return 0x{:0x}", ret_value) } else { String::new() };
-                        println!("{}", format!("[return to 0x{:0x} / {} arguments / pop {} bytes{}]", ret_addr, arg_len, fsp, ret_value_str).bright_green().dimmed());
-                        println!();
-
-                        if $ret_value {
-                            // note: スタックに返り値をプッシュ
-                            push_stack!(usize, ret_value);
-                        }
-
-                        jump_bytecode_to!(ret_addr);
-                    }
+                    next!(inst_ptr, pc, $ty, bytecode_len, ExitStatus::BytecodeAccessViolation)
                 };
             }
 
@@ -517,31 +206,8 @@ impl Interpreter {
                 };
             }
 
-            macro_rules! calc {
-                ($ty:ty, $f:ident$(, $check_divide_by_zero:expr)?) => {
-                    {
-                        let right_term = pop_stack!($ty);
-                        let left_term = pop_stack!($ty);
-
-                        $(
-                            if $check_divide_by_zero && right_term == 0 {
-                                exit!(ExitStatus::DivideByZero);
-                            }
-                        )?
-
-                        let (value, overflowed) = left_term.$f(right_term);
-
-                        if overflowed {
-                            exit!(ExitStatus::ArithmeticOverflow);
-                        }
-
-                        push_stack!($ty, value);
-                    }
-                };
-            }
-
             let tmp_pc = pc;
-            let opcode = next_bytecode!(u8);
+            let opcode = next_prg!(u8);
             let opcode_kind = Opcode::from(opcode);
 
             println!("{}", format!("{} (0x{:0x} at 0x{:0x})", opcode_kind.to_string().to_uppercase(), opcode, tmp_pc).blue());
@@ -551,30 +217,12 @@ impl Interpreter {
             match opcode_kind {
                 Opcode::Nop => (),
                 Opcode::Exit => exit!(ExitStatus::Success),
-                Opcode::Invoke => invoke!(),
-                Opcode::Ret => ret!(false),
-                Opcode::RetVal => ret!(true),
-                Opcode::Push32 => push_stack_next!(u32),
-                Opcode::Push64 => push_stack_next!(u64),
-                Opcode::Pop32 => {
-                    let _ = pop_stack!(u32);
-                },
-                Opcode::Pop64 => {
-                    let _ = pop_stack!(u64);
-                },
-                Opcode::IAdd32 => calc!(u32, overflowing_add),
-                Opcode::IAdd64 => calc!(u64, overflowing_add),
-                Opcode::ISub32 => calc!(u32, overflowing_sub),
-                Opcode::ISub64 => calc!(u64, overflowing_sub),
-                Opcode::IMul32 => calc!(u32, overflowing_mul),
-                Opcode::IMul64 => calc!(u64, overflowing_mul),
-                Opcode::IDiv32 => calc!(u32, overflowing_div, true),
-                Opcode::IDiv64 => calc!(u64, overflowing_div, true),
                 Opcode::Unknown => exit!(ExitStatus::UnknownOpcode),
             }
         }
 
         let exit_status_msg = format!("exit status 0x{:0x} ({})", es, ExitStatus::from(es).to_string());
+
         println!("{}", if es == 0 {
             exit_status_msg.on_bright_black()
         } else {
@@ -582,7 +230,6 @@ impl Interpreter {
         });
 
         free(stack_ptr.sub(sp));
-        free(call_stack_ptr.sub(csp));
 
         return ExitStatus::from(es);
     }
